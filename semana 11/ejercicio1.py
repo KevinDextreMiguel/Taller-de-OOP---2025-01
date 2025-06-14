6e668e843089f7d9772e411d5a2aa55a4a1aa7c2
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2025-06-14 17:09:55 -0500

    Create ejercicio1.py

---
+++ semana 11/ejercicio1.py
@@ -0,0 +1,14 @@
+class Perro:
+  def __init__(self, nombre,edad, raza):
+    self.nombre = nombre
+    self.edad = edad
+    self.raza = raza
+  
+  def comer(self):
+    print('Estoy comiendo')
+
+  def dormir(self):
+    print('Estoy durmiendo')
+  
+  def ladrar(self):
+    print('Estoy ladrando')
