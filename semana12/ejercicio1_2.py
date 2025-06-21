01d6638010b40fabe89c3f2fb4f7abd7ad8d492d
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2025-06-21 18:47:00 -0500

    Create ejercicio1_2.py

---
+++ semana12/ejercicio1_2.py
@@ -0,0 +1,24 @@
+class Data:
+  def __init__(self):
+    self.dataFrame = None
+  
+  def generar(self):
+    meses = ["Enero","Febrero","Marzo","Abril"]
+    ventas = [30500,35600,28300,33900]
+    gastos = [22000,23400,18100,20700]
+    dicc = {"mes":meses,"ventas":ventas,"gasto":gastos}
+    self.dataFrame = pd.DataFrame(dicc)
+  
+  def imprimir(self):
+    print(self.dataFrame)
+  
+  def balance(self):
+    self.dataFrame["ganancia"] = self.dataFrame["ventas"] - self.dataFrame["gasto"]
+    print("\nSe realizò el balance")
+
+  
+objetoData = Data()
+objetoData.generar()
+objetoData.imprimir()
+objetoData.balance()
+objetoData.imprimir()
